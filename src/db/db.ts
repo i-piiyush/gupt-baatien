b0d5e72a37633dfd2c201f2376747ab8e610fb33
i-piiyush <piyushchhabra3578@gmail.com>
2025-12-25 03:04:21 +0530

    connected to DB

---
+++ src/db/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+const connection: connectionObject = {};
+
+export const connectToDb = async (): Promise<void> => {
+  if (connection.isConnected) {
+    console.log("database is already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    console.log(db.connections);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("error connecting to DB: ", error);
+    process.exit(1);
+  }
+};
